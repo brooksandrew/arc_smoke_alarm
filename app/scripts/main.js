da62f4663c4b093cbeadc56b8441770b833252b6
Chris Given <cmgiven@gmail.com>
2015-11-09 19:31:34 -0500

    Scaffolded Leaflet app

---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+/*jslint browser: true*/
+/*global L */
+
+(function (window, document, L, undefined) {
+	'use strict';
+
+	L.Icon.Default.imagePath = 'images/';
+
+	/* create leaflet map */
+	var map = L.map('map', {
+		center: [38.9047, -77.0164],
+		zoom: 10
+	});
+
+	/* add default stamen tile layer */
+	new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+		minZoom: 0,
+		maxZoom: 18,
+		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+	}).addTo(map);
+
+
+	L.marker([38.9047, -77.0164]).addTo(map);
+
+}(window, document, L));
